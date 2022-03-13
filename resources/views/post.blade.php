3825270f5afe788f7b268aa2ca92439d17e2e370
Alkhatep <abdulla.sayed94@gmail.com>
2022-03-13 02:51:14 +0200

    find a composer package for post metadata

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta name="generator" content="HTML Tidy for HTML5 for Linux version 5.8.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" type="text/css" href="/app.css">
+	<meta name="generator" content="HTML Tidy for HTML5 for Linux version 5.8.0">
+	<title>My Blog</title>
+	<link rel="stylesheet" type="text/css" href="/app.css">
 </head>
 <body>
-    <article>
-        <?= $post; ?>
-    </article>
-    <a href="/">Go Back</a>
+<article>
+	<h1>
+		<?= $post->title ?>
+	</h1>
+	
+	
+	<div>
+		<?= $post->body ?>
+	</div>
+	<a href="/">Go Back</a>
+</article>
 </body>
 
 
